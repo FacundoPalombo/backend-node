b483721b1615eb0e4281ff049a6a23557c0f9276
Facundo Palombo <facundo.palombo@gmail.com>
2019-12-18 11:15:43 -0300

    Creating MongoLib for CRUD management with MongoDB collections

--- config/index.js
+++ config/index.js
@@ -2,7 +2,12 @@ require('dotenv').config()
 
 const config = {
   port: process.env.PORT || 3000,
-  env: process.env.NODE_ENV || 'production'
+  env: process.env.NODE_ENV || 'production',
+  dbUser: process.env.DB_USER,
+  dbPassword: process.env.DB_PASSWORD,
+  dbName: process.env.DB_NAME,
+  dbHost: process.env.DB_HOST,
+  dbPort: process.env.DB_PORT
 }
 
 module.exports = { config }
